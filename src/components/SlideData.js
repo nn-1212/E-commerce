9a79ca229128d96392848bfd307fc61229905a76
allison <fidomoon@hotmail.com>
2023-02-14 11:54:01 +0800

    sliderindexitem

---
+++ src/components/SlideData.js
@@ -0,0 +1,16 @@
+export const sliderIndexItem = [
+    {
+        id:1, 
+        img:"https://images.unsplash.com/photo-1675953881447-84b86660f0e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60", 
+        title:"SUMMER SALE", 
+        desc: "夏日折扣", 
+        bg:"f5fafd",
+    }, 
+    {
+        id:2, 
+        img:"https://images.unsplash.com/photo-1675953881447-84b86660f0e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60", 
+        title:"AUTUMN COLLECTION", 
+        desc: "秋季特賣", 
+        bg:"fcf1ed"
+    }, 
+]
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from '@emotion/styled'
 import { ArrowBackIosNewOutlined,ArrowForwardIosOutlined } from '@mui/icons-material';
+import { handleBreakpoints } from '@mui/system';
 
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
     position: relative;
+    overflow: hidden;
 `
 
 const Wrapper = styled.div`
     height: 100%;
+    display: flex;
+    transform: translateX(0vw);
 `
 
 const SlideContainer = styled.div`
@@ -18,6 +22,9 @@ const SlideContainer = styled.div`
     height: 100vh;
     display: flex;
     align-items: center;
+    background-color: #${(props)=>{
+        return props.bg
+    }}
 `
 
 const ImgContainer = styled.div`
@@ -48,6 +55,7 @@ const Button = styled.button`
     padding: 10px;
     font-size: 15px;
     background-color: transparent;
+    cursor: pointer;
 `
 
 
@@ -72,17 +80,24 @@ const Arrow = styled.div`
     }};
     cursor: pointer;
     opacity: 0.5;
+    z-index: 2;
 `
 
 
 const Slider = () => {
+    const [slideIndex, setSlideIndex] = useState(0)
+    
+    const handleClick = (direction)=>{
+
+    }
+
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onclick={handleClick}>
         <ArrowBackIosNewOutlined/>
       </Arrow>
         <Wrapper>    
-            <SlideContainer>    
+            <SlideContainer bg="f5fafd">    
                 <ImgContainer>
                     <Img src="https://images.unsplash.com/photo-1675953881447-84b86660f0e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"/>
                 </ImgContainer>
@@ -92,8 +107,19 @@ const Slider = () => {
                     <Button>SHOP NOW</Button>
                   </InfoContainer>
             </SlideContainer>  
+
+            <SlideContainer bg="fcf1ed">    
+                <ImgContainer>
+                    <Img src="https://images.unsplash.com/photo-1675953881447-84b86660f0e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"/>
+                </ImgContainer>
+                  <InfoContainer>
+                    <Title>SUNMMER SALE</Title>
+                    <Desc>夏日大折扣 上衣3折起、下身5折起</Desc>
+                    <Button>SHOP NOW</Button>
+                  </InfoContainer>
+            </SlideContainer>
        </Wrapper>
-       <Arrow direction="right">
+       <Arrow direction="right" onclick={handleClick}>
         <ArrowForwardIosOutlined/>
        </Arrow> 
     </Container>
