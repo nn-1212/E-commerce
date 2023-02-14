99c3884c4fe5b87752be54945b85906b446d396f
allison <fidomoon@hotmail.com>
2023-02-14 12:22:39 +0800

    slider onclick

--- src/components/SlideData.js
+++ src/components/SlideData.js
@@ -13,4 +13,11 @@ export const sliderIndexItem = [
         desc: "秋季特賣", 
         bg:"fcf1ed"
     }, 
+    {
+        id:3, 
+        img:"https://images.unsplash.com/photo-1675953881447-84b86660f0e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60", 
+        title:"WINTER 90% SALE", 
+        desc: "冬季1折起", 
+        bg:"fcf1ed"
+    }, 
 ]
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -90,12 +90,16 @@ const Slider = () => {
     const [slideIndex, setSlideIndex] = useState(0)
     
     const handleClick = (direction)=>{
-
+        if (direction === "left"){
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
+        } else {
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+        }
     }
 
   return (
     <Container>
-      <Arrow direction="left" onclick={handleClick}>
+      <Arrow direction="left" onclick={()=>handleClick("left")}>
         <ArrowBackIosNewOutlined/>
       </Arrow>
         <Wrapper>    
@@ -113,7 +117,7 @@ const Slider = () => {
             })}
             
        </Wrapper>
-       <Arrow direction="right" onclick={handleClick}>
+       <Arrow direction="right" onclick={()=>handleClick("right")}>
         <ArrowForwardIosOutlined/>
        </Arrow> 
     </Container>
