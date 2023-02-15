d429fb543ce76c9e2fb046fdba81e3b7b1a7d072
allison <fidomoon@hotmail.com>
2023-02-15 15:50:59 +0800

    cart page

---
+++ src/Cart.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+
+const Container = styled.div
+
+const Cart = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default Cart
