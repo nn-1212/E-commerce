41173f0413326a74f5579f1512f8db25d925587b
allison <fidomoon@hotmail.com>
2023-02-15 13:28:56 +0800

    productinfo select

--- src/ProductInfo.js
+++ src/ProductInfo.js
@@ -11,13 +11,54 @@ const Container = styled.div`
 
 `
 
-const Wrapper = styled.div``
-const ImgContainer = styled.div``
-const Img = styled.img``
-const InfoContainer = styled.div``
+const Wrapper = styled.div`
+    display: flex;
+    padding: 50px;
+    
+`
+const ImgContainer = styled.div`
+    flex: 1;
+`
+const Img = styled.img`
+    width: 100%;
+    height: 90vh;
+    object-fit: cover;
+`
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0 50px;
+`
 const Title = styled.h1``
 const Desc = styled.p``
 const Price = styled.span``
+
+
+const FilterContainer = styled.div``
+const Filter = styled.div`
+    display: flex;
+    width: 140px;
+    height: 40px;
+    align-items: center;
+    margin-top: 5px;
+`
+const FilterText = styled.p`
+    margin-right: 5px;
+`
+
+const Color = styled.div`
+    width: 20px;
+    height: 20px;
+    background-color: ${(props)=> props.color};
+    border-radius: 50%;
+    margin: 5px;
+`
+
+const Select = styled.select`
+    width: 80px;
+    height: 20px;
+`
+const Option = styled.option`
+`
 
 const ProductInfo = () => {
   return (
@@ -32,7 +73,24 @@ const ProductInfo = () => {
             <Title>休閒牛仔褲</Title>
             <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime incidunt, vitae libero ex dolor expedita, ab quam rerum cum, et possimus odit. Id quis ipsa, repudiandae quia quaerat maiores aperiam?</Desc>
             <Price> TWD 1280</Price>
-
+            <FilterContainer>
+                <Filter>
+                    <FilterText>color:</FilterText>
+                        <Color color="lightblue"></Color>
+                        <Color color="darkblue"></Color>
+                        <Color color="grey"></Color>
+                </Filter>
+                <Filter>
+                    <FilterText>size:</FilterText>
+                    <Select>
+                        <Option>XS</Option>
+                        <Option>S</Option>
+                        <Option>M</Option>
+                        <Option>L</Option>
+                        <Option>XL</Option>
+                    </Select>
+                </Filter>
+            </FilterContainer>
         </InfoContainer>
       </Wrapper>
       <Newsletter/>
