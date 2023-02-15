5689abe6891904d5f8acf1a829c9105fc5f988d5
allison <fidomoon@hotmail.com>
2023-02-15 10:31:34 +0800

    productlist home

---
+++ src/ProductList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+
+`
+
+const ProductList = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default ProductList
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import ProductList from './ProductList';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <ProductList/>
   </React.StrictMode>
 );
 
